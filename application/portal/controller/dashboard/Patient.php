6dd33dcb3faf9ba2b6ab3c58b44e2c7ba99bfcf7
Yinan Zhou <yinanzhou@me.com>
2018-04-05 00:39:34 -0400

    Implementation of Patient Profile (#14)
    * Create patient table
    
    * Implement display of patient proile
    
    * Add birth year field to patient profile
    
    * Implement backend for patient profile update and retrieval
    
    resolve #8

--- application/portal/controller/dashboard/Patient.php
+++ application/portal/controller/dashboard/Patient.php
@@ -4,6 +4,7 @@ namespace app\portal\controller\dashboard;
 
 use app\common\model\Appointment;
 use app\common\model\Membership;
+use app\common\model\PatientProfile;
 use app\portal\controller\Auth;
 use think\Controller;
 
@@ -21,5 +22,35 @@ class Patient extends Controller {
     if (!Auth::isPatient()) {
       abort(403);
     }
+  }
+
+  public function profile() {
+    if (!Auth::isLogin()) {
+      return Auth::redirectToLogin($this->request);
+    }
+    $this->checkPatientMembership();
+    $uid = Auth::getUserId();
+    $profile = PatientProfile::find($uid);
+    if ($profile == null) {
+      $profile = new PatientProfile;
+      $profile->user_id = $uid;
+      $profile->save();
+      $profile = PatientProfile::find($uid);
+    }
+    if ($this->request->isPost()) {
+      $profile->patient_gender = input('post.patient_gender');
+      $profile->patient_birth_year = input('post.patient_birth_year');
+      $profile->patient_conditions = input('post.patient_conditions');
+      $profile->patient_allergies = input('post.patient_allergies');
+      $profile->patient_medications = input('post.patient_medications');
+      $profile->save();
+      $this->assign('alert','Profile update succeeds.');
+    }
+    $this->assign('active_menu','patient-profile');
+    $genderOptions = PatientProfile::GENDER_OPTIONS;
+    array_unshift($genderOptions, 'Prefer not to answer');
+    $this->assign('gender_options', $genderOptions);
+    $this->assign('p', $profile);
+    return view();
   }
 }
